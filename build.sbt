75b55b9df9915d5458aaee301dca95f0df8557f1
Vegard Harper <vegahar@stud.ntnu.no>
2014-08-31 19:33:07 +0200

    added a redis topology example

--- build.sbt
+++ build.sbt
@@ -7,7 +7,12 @@ scalaVersion := "2.9.1"
 resolvers ++= Seq("clojars" at "http://clojars.org/repo/",
   "clojure-releases" at "http://build.clojure.org/releases")
 
-libraryDependencies += "com.github.velvia" % "scala-storm_2.9.1" % "0.2.2"
+libraryDependencies ++= Seq(
+  "com.github.velvia" % "scala-storm_2.9.1" % "0.2.2",
+  "net.debasishg" % "redisclient_2.9.1" % "2.7"
+)
+
+
 
 
 
--- src/main/scala/RedisTopology.scala
+++ src/main/scala/RedisTopology.scala
@@ -1,3 +1,46 @@
-class RedisTopology {
+import backtype.storm.{LocalCluster, Config}
+import backtype.storm.topology.TopologyBuilder
+import backtype.storm.tuple.{Fields, Tuple}
+import com.redis._
+import storm.scala.dsl.StormBolt
 
+class RedisBolt extends StormBolt(List("word", "redis")) {
+  var redisClient: RedisClient = _
+
+  setup {
+    //this is ip set by vagrant when running redis through boot2docker on windows or osx.
+    redisClient = new RedisClient("192.168.59.103",49153)
+  }
+
+  override def execute(t: Tuple) = t matchSeq  {
+
+    case Seq(word:String) => {
+
+      redisClient.set(word, 1);
+      using anchor t emit (word)
+      t ack
+    }
+
+
+  }
+}
+
+
+object RedisTopology {
+  def main(args: Array[String]) = {
+    val builder = new TopologyBuilder
+
+    builder.setSpout("randsentence", new RandomSentenceSpout)
+    builder.setBolt("split", new SplitSentence).shuffleGrouping("randsentence")
+    builder.setBolt("redis", new RedisBolt).fieldsGrouping("split", new Fields("word"))
+
+    val conf = new Config
+    conf.setDebug(true)
+    conf.setMaxTaskParallelism(3)
+
+    val cluster = new LocalCluster
+    cluster.submitTopology("redis-bolt", conf, builder.createTopology)
+    Thread sleep 10000
+    cluster.shutdown
+  }
 }
